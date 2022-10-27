451da9039f5f92dcd217cc2d2ecb1ed0f3ca617a
meitar <meitar.sela@mail.huji.ac.il>
2022-10-27 21:21:31 +0300

    change main

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 # import training.training
-import parsers.cryo_parser
 
 
 def main():
     # training.training.main()
     print("running cryo parser")
-    parsers.cryo_parser.main()
 
 
 if __name__ == "__main__":
