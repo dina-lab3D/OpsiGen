cfd80e0e85e5ac901ee2fd4a825a138abfb6268d
Meitar Sela <meitar@cs.huji.ac.il>
2023-04-06 20:07:42 +0300

    update sequence

--- pipeline_auto/sequences.py
+++ pipeline_auto/sequences.py
@@ -1,6 +1,8 @@
-import os
-from Bio import PDB
-from Bio.PDB import PDBIO
+"""
+Read the sequences from the fasta file.
+This class has an API for dealing with the 24 amino acids that are most
+related to the wavelength absorption
+"""
 
 AMINO_DICT = {
         "ALA": "A",
@@ -27,6 +29,11 @@ AMINO_DICT = {
 
 
 class Sequences:
+    """
+    Read the sequences from the fasta file.
+    This class has an API for dealing with the 24 amino acids that are most
+    related to the wavelength absorption
+    """
 
     POSITIONS = [
         429,
@@ -64,22 +71,31 @@ class Sequences:
         self.length = len(self.index_mapping)
 
     def get_wavelength_indexes_from_file(self):
+        """
+        Get only the non-NA rhodopsins according to the known wavelengths
+        file
+        """
         with open(self.config["wavelength_file"], "r") as f:
             wl_lines = f.readlines()
 
         indexes = []
-        for i in range(len(wl_lines)):
-            if wl_lines[i] != 'NA\n':
+        for i, wl_line in enumerate(wl_lines):
+            if wl_line != 'NA\n':
                 indexes.append(i)
 
 
         return {i: indexes[i] for i in range(len(indexes))}
 
     def _get_relevant_line(self, protein_index):
-        shifted_index = self.index_mapping[protein_index]
+        """
+        Get the sequence from the .fas file according to the index
+        """
         return self.lines[2 * protein_index + 1].strip()
 
     def get_item(self, protein_index, seq_index):
+        """
+        get rhodopsin[protein_index][seq_index]
+        """
         line = self._get_relevant_line(protein_index)
         amino_index = -1
         seq_running_index = 0
@@ -87,25 +103,26 @@ class Sequences:
 
             if line[seq_running_index] != '-':
                 amino_index += 1
-            
+
             seq_running_index += 1
 
         max_length = len(line.replace('-',''))
-        
+
         if amino_index >= max_length:
             print("Bad sequence!!")
-            exit()
+            return -1
 
-        return amino_index 
+        return amino_index
 
     def get_interesting_positions(self, protein_index):
+        """
+        get the list of 24 amino acids that are corelated with 
+        wavelength absorption
+        """
         res = [self.get_item(protein_index, p) for p in self.POSITIONS]
         for i in range(len(res) - 1):
             if res[i] == res[i+1]:
                 res[i+1] += 1
-        line = self._get_relevant_line(protein_index).replace('-','')
+        self._get_relevant_line(protein_index).replace('-','')
 
         return res
-
-if __name__ == "__main__":
-    main()
